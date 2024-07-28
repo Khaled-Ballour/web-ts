d3277aad67c7b0f8751efc96ca45dd5c71698caa
mohbalor <mohammadballour72@gmail.com>
2024-07-28 20:39:26 +0300

    Model class

---
+++ src/Model.ts
@@ -0,0 +1,24 @@
+import { AxiosPromise } from "axios";
+interface ModelAttributes<T> {
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+  set(value: T): void;
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(eventName: string): void;
+}
+
+export class Model<T> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private sync: Sync<T>,
+    private events: Events
+  ) {}
+}
